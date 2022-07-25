13f558d4f6da7d82b64f9495e196776a185540b2
Juan Pablo <jupagure100@gmail.com>
2022-07-25 18:51:50 -0500

    as

--- README.md
+++
@@ -1,1 +0,0 @@
-# bombrisk2
---
+++ bret/lexicon_en.py
@@ -0,0 +1,16 @@
+class Lexicon:
+    bomb_collected = "Bombas recogídas"
+    boxes_collected = "Número de cuadros recogidos:"
+    boxes_remaining = "Número de cuadros restantes:"
+    boxes_to_collect = "Número de cuadros por recoger:"
+    instructions = "Instrucciones:"
+    no = "No"
+    results = 'Resultados'
+    round_history = "Historial de rondas"
+    round_number = "Ronda N."
+    round_payoff = "Pago por ronda"
+    solve = "Resolver"
+    start = "Empezar"
+    stop = "Stop"
+    yes = "Sí"
+    your_decision = "Su decisión"
